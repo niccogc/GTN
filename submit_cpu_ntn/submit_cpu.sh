c22697ce87796369700bc892b7e805ff1db2c525
niccogc <nicci@dtu.dk>
2026-04-30 21:09:09 +0200

    prego

--- submit_cpu_ntn/submit_cpu.sh
+++ submit_cpu_ntn/submit_cpu.sh
@@ -3,7 +3,7 @@
 #BSUB -J "ntn_grid_full[1-110]%5"
 #BSUB -W 8:00
 #BSUB -n 6
-#BSUB -R "rusage[mem=6GB]"
+#BSUB -R "rusage[mem=8GB]"
 #BSUB -R "span[hosts=1]"
 #BSUB -o logs/%J_%I.out
 #BSUB -e logs/%J_%I.err
