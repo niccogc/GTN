773afcaf696413ad942195839299eb1a25ec7c30
niccogc <nicci@dtu.dk>
2026-05-01 15:06:46 +0200

    n

--- submit_cpu_ntn/submit_cpu.sh
+++ submit_cpu_ntn/submit_cpu.sh
@@ -79,10 +79,10 @@ fi
 
 echo "Task $LSB_JOBINDEX: Dataset=$CURRENT_DATASET, Model=$CURRENT_MODEL, Experiment=$EXPERIMENT"
 
-export OMP_NUM_THREADS=6
-export MKL_NUM_THREADS=6
-export OPENBLAS_NUM_THREADS=6
-export NUMEXPR_NUM_THREADS=6
+export OMP_NUM_THREADS=4
+export MKL_NUM_THREADS=4
+export OPENBLAS_NUM_THREADS=4
+export NUMEXPR_NUM_THREADS=4
 
 python run.py --multirun \
     +experiment=$EXPERIMENT \
