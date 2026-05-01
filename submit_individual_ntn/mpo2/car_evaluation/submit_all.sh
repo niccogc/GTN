1e67ab6804bc8a772c274f40203b827d32ccf5e0
niccogc <nicci@dtu.dk>
2026-05-01 12:14:06 +0200

    speriam

---
+++ submit_individual_ntn/mpo2/car_evaluation/submit_all.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#BSUB -q hpc
+#BSUB -J "ntn_mpo2_car_evaluation[1-5]%20"
+#BSUB -W 1:00
+#BSUB -n 6
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -R "span[hosts=1]"
+#BSUB -o logs/%J_%I.out
+#BSUB -e logs/%J_%I.err
+
+export HOME=/zhome/6b/e/212868
+cd "$HOME/GTN"
+source .venv/bin/activate
+mkdir -p logs
+
+export OMP_NUM_THREADS=6
+export MKL_NUM_THREADS=6
+
+# List of missing configs: "L bond_dim seed"
+CONFIGS=(
+    "4 12 42"
+    "4 12 10090"
+    "4 12 32874"
+    "4 12 47311"
+    "4 12 47303"
+)
+
+IDV=$((LSB_JOBINDEX - 1))
+CONF=(${CONFIGS[$IDV]})
+
+L=${CONF[0]}
+BD=${CONF[1]}
+SEED=${CONF[2]}
+
+echo "Running: model=mpo2, dataset=car_evaluation, L=$L, bd=$BD, seed=$SEED"
+
+python run.py \
+    model=mpo2 \
+    dataset=car_evaluation \
+    model.L=$L \
+    model.bond_dim=$BD \
+    seed=$SEED
