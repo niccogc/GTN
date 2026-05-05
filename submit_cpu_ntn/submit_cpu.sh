12376364b5c4f7f9f779ab80e00ba4b214d17f6c
niccogc <nicci@dtu.dk>
2026-05-05 10:52:23 +0200

    bho

--- submit_cpu_ntn/submit_cpu.sh
+++ submit_cpu_ntn/submit_cpu.sh
@@ -2,7 +2,7 @@
 #BSUB -q hpc
 #BSUB -J "ntn_grid_full[1-210]%20"
 #BSUB -W 8:00
-#BSUB -n 16
+#BSUB -n 10
 #BSUB -R "rusage[mem=500MB]"
 #BSUB -R "span[hosts=1]"
 #BSUB -o logs/%J_%I_newntn.out
@@ -78,10 +78,10 @@ fi
 
 echo "Task $LSB_JOBINDEX: Dataset=$CURRENT_DATASET, Model=$CURRENT_MODEL, Experiment=$EXPERIMENT"
 
-export OMP_NUM_THREADS=6
-export MKL_NUM_THREADS=6
-export OPENBLAS_NUM_THREADS=6
-export NUMEXPR_NUM_THREADS=6
+export OMP_NUM_THREADS=10
+export MKL_NUM_THREADS=10
+export OPENBLAS_NUM_THREADS=10
+export NUMEXPR_NUM_THREADS=10
 
 python run.py --multirun \
     +experiment=$EXPERIMENT \
