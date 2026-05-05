d600a28f5818f9cec0c609f86cd012ada7a81685
niccogc <nicci@dtu.dk>
2026-05-05 08:59:33 +0200

    no test git

---
+++ sync-gtn_test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REL_TARGET="test_outputs"
+TARGET="$SCRIPT_DIR/$REL_TARGET"
+TMP=$(mktemp -d)
+trap 'rm -rf "$TMP"' EXIT
+
+sync_host () {
+  local HOST="$1"
+  local TMPDIR="$TMP/$HOST"
+
+  mkdir -p "$TMPDIR"
+
+  echo "Fetching from $HOST..."
+    ssh "$HOST" "tar -I zstd -cf - -C ~/GTN \"$REL_TARGET\"" \
+    | tar -I zstd -xf - -C "$TMPDIR"
+
+  echo "Merging from $HOST..."
+
+  rsync -a "$TMPDIR/outputs/" "$TARGET/"
+}
+
+# sync_host titans &
+sync_host hpc &
+
+wait
+echo "Done."
